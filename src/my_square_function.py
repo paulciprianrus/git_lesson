8907c41b81c98a33836e62599db073412ee78c5e
paulciprianrus <paul.ciprian.rus@gmail.com>
2020-07-02 14:06:40 +0300

    update my_square_function: adding more description to the functions

--- src/my_square_function.py
+++ src/my_square_function.py
@@ -1,11 +1,13 @@
 
 def my_square(y):
 	""" this is a function that returns square value
+	it uses * operator
 	"""	
 	return(y * y)
 
 def my_square2(x):
 	""" this is the second square function
+	it uses ** operator
 	"""
 	return(x ** 2)
 
